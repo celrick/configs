080512ecf14a1883d646b00fcdd59d92e9e96fce
Conor (cusith) <conorelrick@gmail.com>
2024-02-15 04:13:54 +0000

    Fixed start of ps1 to match colour scheme

--- ps1_slurm.sh
+++ ps1_slurm.sh
@@ -8,5 +8,5 @@ GREEN="\[\e[1;32m\]"
 C2="\[\e[0;35m\]"
 NORM="\[\e[0m\]"
 GRAYY="\[\e[1;37m\]"
-PS1="\n  ┌(\$(if [[ \$? == 0 ]]; then echo \"$GREEN^.^\"; else echo \"${RED}O_O\"; fi)$GRAYY)-($C1\u@\h$GRAYY)-(${C1}slurm jobs:\$(squeue -u $USER -h | wc -l)$GRAYY)-($C1\@$GRAYY)-(${C2}wd:\w$GRAYY)\n\$(if [ ${EUID} -eq 0 ]; then echo \"$RED\"; else echo \"$GRAY\"; fi) $NORM └─$"
+PS1="\n $GRAYY ┌(\$(if [[ \$? == 0 ]]; then echo \"$GREEN^.^\"; else echo \"${RED}O_O\"; fi)$GRAYY)-($C1\u@\h$GRAYY)-(${C1}slurm jobs:\$(squeue -u $USER -h | wc -l)$GRAYY)-($C1\@$GRAYY)-(${C2}wd:\w$GRAYY)\n\$(if [ ${EUID} -eq 0 ]; then echo \"$RED\"; else echo \"$GRAY\"; fi) $GRAYY └─$ $NORM"
 
